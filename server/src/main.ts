a3d9852173320c146069ead14339335e1d20c015
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-04-14 10:57:00 +0200

    feat: server starter boilerplate with NestJS in place, connected to .env file

---
+++ server/src/main.ts
@@ -0,0 +1,9 @@
+require('dotenv').config({ path: '../.env' });
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {cors: true});
+  await app.listen(process.env.NEST_PORT || 3001);
+}
+bootstrap();
