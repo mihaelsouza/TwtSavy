b23f4153c95349ceb6f63453e818cc8b46ba0eee
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-04-15 12:37:38 +0200

    refactor: extracted get tweets logic from routes to a single function

--- server/src/analyze/analyze.controller.ts
+++ server/src/analyze/analyze.controller.ts
@@ -9,6 +9,21 @@ import { Controller, Get, Param } from '@nestjs/common';
 export class AnalyzeController {
   private tweets: Tweet[] = [];
 
+  private getTweets = async(search: string | number, func: Function): Promise<void> => {
+    let twitterResponse = await func.call(this.twitterService, search, '');
+    this.tweets = filterNonEnglish(twitterResponse.data.data);
+
+    let count = 0;
+    let page: string = twitterResponse.data.meta.next_token || '';
+    while (page && count < 400) {
+      twitterResponse = await func.call(this.twitterService, search, page);
+      this.tweets = this.tweets.concat(filterNonEnglish(twitterResponse.data.data));
+
+      count += twitterResponse.data.meta.result_count
+      page = twitterResponse.data.meta.next_token;
+    }
+  };
+
   constructor (
     private aiService: AnalyzeService,
     private twitterService: TwitterApiService
@@ -16,22 +31,7 @@ export class AnalyzeController {
 
   @Get('hashtag/:query')
     async getHashtag(@Param('query') query: string): Promise<ModelData[]> {
-      let twitterResponse = await this.twitterService.getHashtagQuery(query, '');
-      this.tweets = twitterResponse.data.data;
-
-      let page: string = twitterResponse.data.meta.next_token || '';
-      if (page !== '') {
-        let count = 0;
-        while (page && count < 400) {
-          twitterResponse = await this.twitterService.getHashtagQuery(query, page);
-          this.tweets = this.tweets.concat(twitterResponse.data.data);
-
-          count += twitterResponse.data.meta.result_count
-          page = twitterResponse.data.meta.next_token;
-          console.log(count, page, this.tweets.length)
-        }
-      }
-
+      await this.getTweets(query, this.twitterService.getHashtagQuery);
       const modelResponse = await this.aiService.getSentiment(tweetToModelData(this.tweets));
       return modelResponse.data;
     }
@@ -41,22 +41,7 @@ export class AnalyzeController {
       let twitterResponse = await this.twitterService.getUserID(username);
       const userID = twitterResponse.data.data.id;
 
-      twitterResponse = await this.twitterService.getUserTimeline(userID, '');
-      this.tweets = filterNonEnglish(twitterResponse.data.data);
-
-      let page: string = twitterResponse.data.meta.next_token || '';
-      if (page !== '') {
-        let count = 0;
-        while (page && count < 400) {
-          twitterResponse = await this.twitterService.getUserTimeline(userID, page);
-          this.tweets = this.tweets.concat(filterNonEnglish(twitterResponse.data.data));
-
-          count += twitterResponse.data.meta.result_count
-          page = twitterResponse.data.meta.next_token;
-          console.log(count, page, this.tweets.length)
-        }
-      }
-
+      await this.getTweets(userID, this.twitterService.getUserTimeline);
       const modelResponse = await this.aiService.getSentiment(tweetToModelData(this.tweets));
       return modelResponse.data;
     }
@@ -66,22 +51,7 @@ export class AnalyzeController {
       let twitterResponse = await this.twitterService.getUserID(username);
       const userID = twitterResponse.data.data.id;
 
-      twitterResponse = await this.twitterService.getUserMentions(userID, '');
-      this.tweets = filterNonEnglish(twitterResponse.data.data);
-
-      let page: string = twitterResponse.data.meta.next_token || '';
-      if (page !== '') {
-        let count = 0;
-        while (page && count < 400) {
-          twitterResponse = await this.twitterService.getUserMentions(userID, page);
-          this.tweets = this.tweets.concat(filterNonEnglish(twitterResponse.data.data));
-
-          count += twitterResponse.data.meta.result_count
-          page = twitterResponse.data.meta.next_token;
-          console.log(count, page, this.tweets.length)
-        }
-      }
-
+      await this.getTweets(userID, this.twitterService.getUserMentions);
       const modelResponse = await this.aiService.getSentiment(tweetToModelData(this.tweets));
       return modelResponse.data;
     }
