203f15bb3e1ba6d762cd98072604fd9a3ae70503
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-04-15 11:41:16 +0200

    feat: added a function to filter non-english tweets when necessary

--- server/src/analyze/utilities/utils.ts
+++ server/src/analyze/utilities/utils.ts
@@ -1,6 +1,10 @@
 import { ModelData } from './model.data.interface';
 import { Tweet } from './tweet.interface';
 
+export function filterNonEnglish(tweets: Tweet[]): Tweet[] {
+  return tweets.filter((tweet) => tweet.lang === 'en');
+};
+
 export function tweetToModelData(tweets: Tweet[]): ModelData[] {
   return tweets.map((tweet) => {
     return {
