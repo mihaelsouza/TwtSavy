56b55285157434310b1444fd6063cac3adc33f42
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-04-15 15:14:21 +0200

    feat: connected the backend with mongoDB using @nestjs/mongoose

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnalyzeModule } from './analyze/analyze.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AnalyzeModule, UsersModule],
+  imports: [
+    MongooseModule.forRoot(`${process.env.NEST_MONGO_HOST}:${process.env.NEST_MONGO_PORT}/${process.env.NEST_MONGO_DB}`),
+    AnalyzeModule,
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
