d0b0e179873defebf34700dc037716d3268d8dd1
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-04-15 15:16:36 +0200

    feat: added get and post routes for User with the database handling

--- server/src/users/services/users.service.ts
+++ server/src/users/services/users.service.ts
@@ -1,4 +1,21 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { UserDTO } from '../utilities/create-user.dto';
+import { User, UserDocument } from '../utilities/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor (
+    @InjectModel(User.name) private userModel: Model<UserDocument>
+  ) {}
+
+  async createUser(userData: UserDTO): Promise<User> {
+    const newUser = new this.userModel(userData);
+    return await newUser.save();
+  }
+
+  async findUser(property: string): Promise<User> {
+    return await this.userModel.findOne({ email: property });
+  }
+};
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UserDTO } from './utilities/create-user.dto';
+import { UsersService } from './services/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor ( private usersService: UsersService ) {}
+
+  @Get(':email')
+    async getUser(@Param('email') email: string): Promise<UserDTO | string> {
+      const user = await this.usersService.findUser(email);
+      return user ? user : 'No user with that email address.';
+    }
+
+  @Post()
+    async createUser(@Body() newUser: UserDTO): Promise<UserDTO | string> {
+      const user = await this.usersService.findUser(newUser.email);
+      return user ? 'This email address is already in use.' : await this.usersService.createUser(newUser);
+    }
+};
