0d9d56986c56104f2b1ca4d0738843310912bb05
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-04-19 20:51:32 +0200

    feat: added password hashing using bcrypt to user creation

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,19 +1,25 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UserDTO } from './utilities/create-user.dto';
+import { hash, compare } from 'bcrypt';
 
 @Controller('users')
 export class UsersController {
   constructor ( private usersService: UsersService ) {}
 
-  @Get(':email')
-    async getUser(@Param('email') email: string): Promise<UserDTO | string> {
+  @Post()
+    async getUser(@Body() {email, password}: {email: string, password: string}): Promise<string | boolean> {
       const user: UserDTO | null = await this.usersService.findUser(email);
-      return user ? user : 'No user with that email address.';
+
+      if (user) {
+        const match = await compare(password, user.password);
+        return match ? true : false;
+      } else return 'Error: e-mail not registered!';
     }
 
-  @Post()
+  @Post('add')
     async createUser(@Body() newUser: UserDTO): Promise<UserDTO | string> {
+      newUser.password = await hash(newUser.password, 10);
       const user: UserDTO | null = await this.usersService.findUser(newUser.email);
       return user ? 'This email address is already in use.' : await this.usersService.createUser(newUser);
     }
