3ac3b817f5aeedc2772adced8e305d6a0f8c95a3
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-04-21 16:05:37 +0200

    feat: typing route prop into DashboardView

--- client/utilities/types.tsx
+++ client/utilities/types.tsx
@@ -1,14 +1,17 @@
 import { UserDTO } from './user-dto';
+import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
+// General Screens type definition
 export type StackParamList = {
   StackScreen: undefined;
   LandingView: undefined;
   UserLogin: undefined;
   UserRegistration: undefined;
-  DashboardView: {user: UserDTO} | undefined;
+  DashboardView: {user: UserDTO};
 };
 
+// Each Screen individual definition
 export type LandingViewNavigationProp = StackNavigationProp<
   StackParamList,
   'LandingView'
@@ -29,6 +32,10 @@ export type DashboardViewNavigationProp = StackNavigationProp<
   'DashboardView'
 >;
 
+// Route definitions - for when passing parameters while navigating
+export type DashboardViewRouteProp = RouteProp<StackParamList, 'DashboardView'>;
+
+// Additional types
 export type Form = {
   [key: string]: string | undefined;
   fullName: string;
