06ef93df4696a0782cf1ef9cae59b1ddc7dd63e1
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-04-21 08:48:26 +0200

    feat: added function to filter out model response of empty string input

--- server/src/analyze/utilities/utils.ts
+++ server/src/analyze/utilities/utils.ts
@@ -1,11 +1,15 @@
 import { ModelData } from './model.data.interface';
 import { Tweet } from './tweet.interface';
 
-export function filterNonEnglish(tweets: Tweet[]): Tweet[] {
+export function filterNonEnglish (tweets: Tweet[]): Tweet[] {
   return tweets.filter((tweet) => tweet.lang === 'en');
 };
 
-export function tweetToModelData(tweets: Tweet[]): ModelData[] {
+export function filterEmptyModelReturns (data: ModelData[]): ModelData[] {
+  return data.filter((data) => data.sentiment !== 'invalid');
+};
+
+export function tweetToModelData (tweets: Tweet[]): ModelData[] {
   return tweets.map((tweet) => {
     return {
       text: tweet.text,
