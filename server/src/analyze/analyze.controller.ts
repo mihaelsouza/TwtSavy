46fc2033a60fd6df8a6626d9e301230400df551c
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-04-21 08:49:46 +0200

    feat: added filterEmptyModelReturns and abstracted the logic into a function

--- server/src/analyze/analyze.controller.ts
+++ server/src/analyze/analyze.controller.ts
@@ -1,4 +1,4 @@
-import { tweetToModelData, filterNonEnglish } from './utilities/utils';
+import { tweetToModelData, filterNonEnglish, filterEmptyModelReturns } from './utilities/utils';
 import { ModelData } from './utilities/model.data.interface';
 import { Tweet } from './utilities/tweet.interface';
 
@@ -10,27 +10,33 @@ import { AxiosResponse } from 'axios';
 
 @Controller('analyze')
 export class AnalyzeController {
-  private tweets: Tweet[] = [];
+  private getTwitterId: Function = async (username: string): Promise<number> => {
+    let twitterResponse: AxiosResponse = await this.twitterService.getUserID(username);
+    return twitterResponse.data.data.id;
+  }
 
-  private getTweets: Function = async (search: string | number, func: Function): Promise<void> => {
+  private getTweets: Function = async (search: string | number, func: Function): Promise<Tweet[]> => {
     let twitterResponse: AxiosResponse = await func.call(this.twitterService, search, '');
-    this.tweets = filterNonEnglish(twitterResponse.data.data);
+    let tweets: Tweet[] = filterNonEnglish(twitterResponse.data.data);
 
     let count: number = 0;
     let page: string = twitterResponse.data.meta.next_token || '';
     while (page && count < 400) {
       twitterResponse = await func.call(this.twitterService, search, page);
-      this.tweets = this.tweets.concat(filterNonEnglish(twitterResponse.data.data));
+      tweets = tweets.concat(filterNonEnglish(twitterResponse.data.data));
 
       count += twitterResponse.data.meta.result_count
       page = twitterResponse.data.meta.next_token;
     }
+
+    return tweets;
   };
 
-  private getTwitterId: Function = async (username: string): Promise<number> => {
-    let twitterResponse: AxiosResponse = await this.twitterService.getUserID(username);
-    return twitterResponse.data.data.id;
-  }
+  private getSentiment: Function = async (tweets: Tweet[]): Promise<ModelData[]> => {
+    const modelResponse: AxiosResponse = await this.aiService.getSentiment(tweetToModelData(tweets));
+    const modelData: ModelData[] = modelResponse.data;
+    return filterEmptyModelReturns(modelData);
+  };
 
   constructor (
     private aiService: AnalyzeService,
@@ -39,24 +45,21 @@ export class AnalyzeController {
 
   @Get('hashtag/:query')
     async getHashtag(@Param('query') query: string): Promise<ModelData[]> {
-      await this.getTweets(query, this.twitterService.getHashtagQuery);
-      const modelResponse: AxiosResponse = await this.aiService.getSentiment(tweetToModelData(this.tweets));
-      return modelResponse.data;
+      const tweets: Tweet[] = await this.getTweets(query, this.twitterService.getHashtagQuery);
+      return await this.getSentiment(tweets);
     }
 
   @Get('timeline/:username')
     async getTimeline(@Param('username') username: string): Promise<ModelData[]> {
       const userID: number = await this.getTwitterId(username);
-      await this.getTweets(userID, this.twitterService.getUserTimeline);
-      const modelResponse: AxiosResponse = await this.aiService.getSentiment(tweetToModelData(this.tweets));
-      return modelResponse.data;
+      const tweets: Tweet[] = await this.getTweets(userID, this.twitterService.getUserTimeline);
+      return await this.getSentiment(tweets);
     }
 
   @Get('mentions/:username')
     async getMentions(@Param('username') username: string): Promise<ModelData[]> {
       const userID: number = await this.getTwitterId(username);
-      await this.getTweets(userID, this.twitterService.getUserMentions);
-      const modelResponse: AxiosResponse = await this.aiService.getSentiment(tweetToModelData(this.tweets));
-      return modelResponse.data;
+      const tweets: Tweet[] = await this.getTweets(userID, this.twitterService.getUserMentions);
+      return await this.getSentiment(tweets);
     }
 }
