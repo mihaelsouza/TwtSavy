293b644cb30b2bb46adf215c00bb389ba9f3c816
Mihael Machado de Souza <mihaelsouza@gmail.com>
2021-04-26 10:39:56 +0200

    feat: OutcomesView renders list of dominant words based on positive/negative case

--- client/screens/OutcomesView.tsx
+++ client/screens/OutcomesView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import Header from '../containers/Header';
 import Button from '../components/Button';
@@ -7,6 +8,7 @@ import Background from '../containers/Background';
 import ContentBox from '../containers/ContentBox';
 
 import { OutcomesViewNavigationProp, OutcomesViewRouteProp } from '../utilities/types';
+import { useAppSelector } from '../redux/hooks';
 
 interface Props {
   route: OutcomesViewRouteProp,
@@ -14,17 +16,45 @@ interface Props {
 };
 
 const OutcomesView: React.FC<Props> = ({ navigation, route }) => {
-  const { outcome } = route.params;
+  const { outcome, colors } = route.params;
+  const outcomesArray = useAppSelector(state => state.wordFrequency[outcome]);
 
   return (
     <Background>
-      <Header />
-      <ContentBox>
-      </ContentBox>
+      <LinearGradient colors={[colors[0], colors[1]]} style={styles.gradient}>
+        <Header />
+        <ContentBox>
+          <Text style={styles.text}>We found words such as:</Text>
+          <Text style={styles.highlight}>{
+            outcomesArray.words
+              .filter((item) => item.length > 4)
+              .slice(0,10)
+              .join(', ')
+          }</Text>
+          <Text style={styles.text}>{`in ${outcome} tweets, and it represented ${outcomesArray.frequency}% of the total tweets.`}</Text>
+        </ContentBox>
+        <Button buttonLabel="Back to Resutls" onPress={() => navigation.navigate('ResultsView')}></Button>
+      </LinearGradient>
     </Background>
   )
 }
 
 export default OutcomesView;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'Raleway-Regular',
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  highlight: {
+    fontFamily: 'FrederickatheGreat-Regular',
+    fontSize: 22,
+    textAlign: 'center',
+    marginVertical: 5,
+  },
+  gradient: {
+    height: '100%',
+    alignItems: 'center',
+  },
+});
